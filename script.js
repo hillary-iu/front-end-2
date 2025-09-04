6702e641c138be29275050a65d7eae5f75f69fbc
hillary-iu <hillary.eduarda@estudante.ifro.edu.br>
2025-09-04 10:21:40 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const paragrafo = window.document.querySelector('#pInicial');
+const lista = document.querySelectorAll('li');
+
+console.log(paragrafo.textContent);
+paragrafo.textContent = 'novo conteudo';
+console.log(lista.length);
+console.log(link.getattribute('heref'));
+
+
+link.setattribute('href','https://www.ifro.edu.br' );
+link.setattribute('target', 'blank');
+
+paragrafo.style.color = 'laranja';
+
+addEventListener('click', (ev)=> {
+    alert('clicou!');
+    ev.target.style.backgraundcolor = 'roxo';
+})
+
+
