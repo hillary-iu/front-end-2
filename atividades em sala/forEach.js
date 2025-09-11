0da2501a29b52fdce393f7cfb01a1033e6ae1764
hillary-iu <hillary.eduarda@estudante.ifro.edu.br>
2025-09-11 10:36:12 -0400

    lalalala

---
+++ atividades em sala/forEach.js
@@ -0,0 +1,5 @@
+const frutas = ['banana', 'laranja', 'pocam', 'maça', 'uva', 'amora'];
+
+frutas.forEach( (fruta, i, fts) => {
+    console.log(i , '-' ,fruta, fts)
+})
--- atividades em sala/script.js
+++ atividades em sala/script.js
@@ -2,6 +2,8 @@ const lcond = document.querySelector('#lcond');
 const input = document.querySelector('input');
 const p = document.querySelector('p');
 const prodAdd = document.querySelector('#prodAdd');
+const lista = document.querySelectorAll('li');
+const prodComp = document.querySelector('#prodComp');
 
 
 lcond.addEventListener('mauseover', () => {
@@ -14,18 +16,31 @@ lcond.addEventListener('mouseout', () => {
     lcond.style.backgraundColor = '';
 });
 
-input.addEventListener('input',() =>  {
+input.addEventListener('change',() =>  {
     console.log(input.value);
     p.textContent = "sua busca: " + input.value;
-});
+})
 
 prodAdd.addEventListener('mousemove' , () => {
     let r = parseInt(Math.random() * 255);
-    let g = parseInt(Math.random() * 255);
+    let g = parseInt(Math.random() * 255 );
     let b = parseInt(Math.random() * 255);
-    let cor = `rgb(${r}, $(g), ${b})`
+    let cor = `rgb(${r}, ${g}, ${b})`
     console.log(cor);
+    document.body.style.backgraundColor = cor;
+    prodAdd.style.backgraundColor = cor;
 })
 
-
-Math.random
+lista.forEach((item)  => {
+    //console.log(item)
+    item.addEventListener( 'click', (ev) => {
+        console.log(ev.target.innerText)
+        console.log(ev.target.parentElement)
+        pai = ev.target.parantElement;
+        filho = ev.target;
+        if (pai === prodComp){
+            prodAdd.appendChild(filho);
+        }
+        
+    })
+})
